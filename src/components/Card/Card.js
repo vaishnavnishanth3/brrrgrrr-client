84f7af0ad6759e795060f87f8bdbf46e1598c19b
Vaishnav Nishanth A V <vaishnavnishanth3@gmail.com>
2024-04-18 22:50:47 +0530

    Update Card.js

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,36 +1,35 @@
 import React from "react"
+import axios from "axios"
 
 import "./Card.css"
 
 export default function Card(props) 
 {
-    function handleClick () {
-        
+    const title = props.title;
+    
+    function handleClick (e) {
+        console.log("Clicked")
+        const userID = JSON.parse(localStorage.getItem('user')).userData.userId
+        const URL = `http://localhost:3001/orders/${userID}`
+        e.preventDefault();
+        axios.post(URL ,{
+            id: userID,
+            title
+        })
+        .then(()=> console.log("Order Submitted"))
+        .catch(error => console.log(error))
     }
 
     return (
         <div className="Card">
             <div className="card-image">
-                <img 
-                    src={props.image} 
-                    alt={props.title}/>
+                <img src={props.image} alt={props.title}/>
             </div>
             
             <div className="card-body">
-                <h5>
-                    {props.title}
-                </h5>
-                
-                <p>
-                    <span className="card-price">
-                        ₹{props.price}
-                    </span>
-                </p>
-                <button 
-                    className="order-button" 
-                    onClick={handleClick}>
-                        Order
-                </button>
+                <h5>{props.title}</h5>
+                <p> <span className="card-price">₹{props.price}</span> </p>
+                <button className="order-button" onClick={handleClick}>Order</button>
             </div>
         </div>
     )
